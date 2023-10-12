c6a3449b8d821bcc1a3550b5e916f24244c3bb09
mmnos <mnos9897@gmail.com>
2023-10-11 20:23:33 -0400

    updated gitignore and fix dev dep warning

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,7 @@ export const App = () => {
   console.log('inside app');
   return (
     <div className="bg-blue-300 p-5">
-      <h1 className="text-3xl font-bold underline">DripDrop</h1>
+      <h1 className="text-3xl font-bold">DripDrop</h1>
       <input name="myInput" />
       <button>Hello</button>
     </div>
