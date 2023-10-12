e0c97ef9b0bbe7a51c66ba53fdbf83bd0cc57483
cm-goodwin <cmgoodwin96@gmail.com>
2023-10-11 22:05:31 -0400

    Input bar changes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,8 +5,18 @@ export const App = () => {
   console.log('inside app');
   return (
     <div className="bg-blue-300 p-5">
-      <h1 className="text-3xl font-bold">DripDrop</h1>
-      <input name="myInput" />
+      <h1 className="text-4xl font-bold  ">DripDrop</h1>
+
+      <form action="" className="peer relative z-10 w-max mx-auto">
+        <input
+          type="search"
+          className="rounded-full hover:ring 
+          focus:outline-none 
+          focus:ring 
+          focus:ring-black-500 pl-4 
+          boxContent h-10 w-80 "
+        />
+      </form>
       <button>Hello</button>
     </div>
   );
